3cb62ffdc34f73c31673e8e66fdc777527fda599
asplicity <ticobeier@gmail.com>
2024-05-15 15:27:50 +0200

    H

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 
 Console.WriteLine("Wie ist dein Vorname?");
-var First_name = Console.ReadLine();
+var First_name = Console.ReadLine() ?? string.Empty;
 Console.WriteLine("Wie ist dein Nachname");
-var second_name = Console.ReadLine();
+var second_name = Console.ReadLine() ?? string.Empty;
 
-#pragma warning disable
+
 Console.WriteLine($"Herzlich Wilkommen, {First_name} {second_name}. Bitte setze dich an den nächsten Tisch. Dein Name beinhaltet {First_name.Length + second_name.Length} Zeichen.");
 Console.WriteLine("zum beenden taste drücken");
 Console.ReadLine();
--- obj/Debug/net8.0/Aufgabe-session-2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Aufgabe-session-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aufgabe-session-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869bdbfed76d41cd425d216fca814f12a89c6de7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e6a4a70debd247cd920ccb18158a1b7c3328a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aufgabe-session-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aufgabe-session-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
