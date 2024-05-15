23fb54022dec91b4c881714d53b0289255619a69
asplicity <ticobeier@gmail.com>
2024-05-15 14:58:11 +0200

    done

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.WriteLine("Wie ist dein Vorname?");
+var First_name = Console.ReadLine();
+Console.WriteLine("Wie ist dein Nachname");
+var second_name = Console.ReadLine();
+
+#pragma warning disable
+Console.WriteLine($"Herzlich Wilkommen, {First_name} {second_name}. Bitte setze dich an den nächsten Tisch. Dein Name beinhaltet {First_name.Length + second_name.Length} Zeichen.");
